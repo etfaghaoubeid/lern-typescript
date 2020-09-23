626a1a85793f996615bc8907932fc9ae6e5385be
etfaghaoubeid <etfaghaoubeid@gmail.com>
2020-09-23 09:01:56 +0000

    init decorator

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,1 @@
 "use strict";
-// init generic
-let promise = new Promise((resolve, reject) => {
-    setTimeout(resolve('promise resolved'), 1000);
-});
-promise.then(data => {
-    console.log(data);
-});
-function mergeObj(obj1, obj2) {
-    return Object.assign(obj1, obj2);
-}
-let r = mergeObj({ namee: "atigh", position: "developer" }, { name: "Mouvid" });
-console.log(r);
-function join(param1) {
-}
